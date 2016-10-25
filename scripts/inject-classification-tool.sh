6b4b55ef4b7b4a8e2d856999a8daeb65e5870e48
Josh Partlow <joshua.partlow@puppetlabs.com>
2016-10-25 10:02:05 -0700

    Install rbac tool as well
    and add an option to skip installing rsync

--- scripts/inject-classification-tool.sh
+++ scripts/inject-classification-tool.sh
@@ -1,17 +1,33 @@
 set -e
 #set -x
 
+INSTALL_RSYNC='true'
+
+while getopts s name; do
+  case "$name" in
+    s)
+      INSTALL_RSYNC='false'
+      ;;
+  esac
+  shift
+done
+
 . ./common.sh
 
 target=${1}
+tool=${2:-all}
 
 if [ -z "${target}" ]; then
-    echo "Usage: inject-classifier-tool.sh <puppet-master-hostname>"
+    echo "Usage: inject-classifier-tool.sh [-s] <puppet-master-hostname> [tool]"
+    echo "  (the tool argument may be 'all', 'classification' or 'rbac')"
+    echo "  -s to skip installing rsync"
     echo "Example: inject-classifier-tool.sh pe-201530-master.puppetdebug.vlan"
     exit 1
 fi
 
-ensure_rsync $PLATFORM $target
+if [ "$INSTALL_RSYNC" = "true" ]; then
+  ensure_rsync $PLATFORM $target
+fi
 
 echo "VER: ${VER}"
 if (( ${VER%%.*} < 4 )); then
@@ -19,5 +35,11 @@ if (( ${VER%%.*} < 4 )); then
 else
     ruby_bin='/opt/puppetlabs/puppet/bin'
 fi
-ssh_on $target "${ruby_bin?}/gem install puppetclassify"
-rsync_on $target ./classification-tool.rb /usr/local/bin
+
+if [ "$tool" = "all" -o "$tool" = "classification" ]; then
+  ssh_on $target "${ruby_bin?}/gem install puppetclassify"
+  rsync_on $target ./classification-tool.rb /usr/local/bin
+fi
+if [ "$tool" = "all" -o "$tool" = "rbac" ]; then
+  rsync_on $target /s/scripts/create_local_user.rb /usr/local/bin
+fi
