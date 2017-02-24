fee0bff0337804195ed8718b2edca3d909084b83
Josh Partlow <joshua.partlow@puppetlabs.com>
2017-02-23 16:59:09 -0800

    Allow comparison from arbitrary revisions

--- scripts/compare-conflicts.sh
+++ scripts/compare-conflicts.sh
@@ -1,14 +1,32 @@
 #! /bin/bash
-set -x
+#set -x
 
-file=${1:?}
-current=${2:-flanders}
-older=${3:-2016.5.x}
+file=${1}
+current=${2:-glisan}
+older=${3:-flanders}
+
+if [ -z "$file" ]; then
+  echo "Usage: compare-conflicts.sh file [current_version] [older_version]"
+  echo "  current_version currently defaults to : $current"
+  echo "  older_version currently defaults to   : $older"
+  exit 1
+fi
 
 tmp=/home/jpartlow/work/tmp
 current_out="$tmp/file.$current"
 older_out="$tmp/file.$older"
 
-git show "upstream/$older":"$file" > "$older_out"
-git show "upstream/$current":"$file" > "$current_out"
+current_ref="upstream/$current"
+if ! git rev-parse "$current_ref"; then
+  current_ref="$current"
+fi
+
+older_ref="upstream/$older"
+if ! git rev-parse "$older_ref"; then
+  older_ref="$older"
+fi
+
+
+git show "$older_ref":"$file" > "$older_out"
+git show "$current_ref":"$file" > "$current_out"
 vimdiff "$older_out" "$current_out"
