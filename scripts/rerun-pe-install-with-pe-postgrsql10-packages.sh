003b00bfd6782edbb4ac77d88d15bc53b24a7c02
Josh Partlow <joshua.partlow@puppetlabs.com>
2019-01-08 01:14:58 +0000

    (maint) Add handling for Ubuntu/Sles in pe install rerun script
    Handles repo recreating and metadata signing. This was clipped from
    frankenbuilder/beaker helper scripts.

--- scripts/rerun-pe-install-with-pe-postgrsql10-packages.sh
+++ scripts/rerun-pe-install-with-pe-postgrsql10-packages.sh
@@ -12,17 +12,19 @@
 set -x
 set -e
 
-platform=$(puppet facts | grep platform_tag | grep -oE '[a-z0-9_]+-[a-z0-9_.]+-[a-z0-9_]')
+platform=$(/opt/puppetlabs/bin/puppet facts | grep platform_tag | grep -oE '[a-z0-9_]+-[a-z0-9_.]+-[a-z0-9_]+')
 os=${platform%%-*}
-osver=${platform#*-"${platform%-*}"}
+_os_name_and_ver=${platform%-*}
+os_ver=${_os_name_and_ver#*-}
+os_arch=${platform##*-}
 if [ "$os" == 'ubuntu' ]; then
-  if [ "$osver" == '18.04' ]; then
-    oslabel=bionic
+  if [ "$os_ver" == '18.04' ]; then
+    codename=bionic
   else
-    oslabel=precise
+    codename=xenial
   fi
 fi
-pe_dir="/root/$(ls -d puppet-enterprise-2019.1.0*x86_64)"
+pe_dir="/root/$(ls -d puppet-enterprise-2019.1.0*"${os_arch}")"
 pe_package_dir="${pe_dir}/packages/${platform}"
 
 cd /root || exit 1
@@ -39,10 +41,11 @@ set -e
 
 case "$os" in
   el)
-    yum install -y tree vim rpm-build rpm-sign createrepo expect
+
+    yum install -y tree vim rpm-sign createrepo expect
 
     # cp my dev postgres10 packages into the pe tarball
-    cp "/jpartlow-src/puppet-enterprise-vanagon/output/el/$osver/products/x86_64/*.rpm" "${pe_package_dir}"
+    cp /jpartlow-src/puppet-enterprise-vanagon/output/"$os"/"$os_ver"/products/x86_64/*.rpm "${pe_package_dir}"
 
     # ensure pub key is imported into rpm
     rpm --import gpg/GPG-KEY-frankenbuilder.pub
@@ -76,24 +79,84 @@ EOF
       createrepo --update .
     popd
     ;;
-  ubuntu)
-    apt install -y tree vim
-
-    cp /jpartlow-src/puppet-enterprise-vanagon/output/deb/$oslabel/products/x86_64/*.deb "${pe_package_dir}"
-
-    cat gpg/GPG-KEY-Frankenbuilder.pub | apt-key add -
-
-
-    ;;
   sles)
-    exit 2
+    zypper install -y tree vim createrepo
+
+    # cp my dev postgres10 packages into the pe tarball
+    cp /jpartlow-src/puppet-enterprise-vanagon/output/"$os"/"$os_ver"/products/x86_64/*.rpm "${pe_package_dir}"
+
+    # ensure pub key is imported into rpm
+    rpm --import gpg/GPG-KEY-frankenbuilder.pub
+
+    # rebuild repo
+    pushd "${pe_package_dir}"
+      createrepo --update .
+    popd
+
+    # sign repomd
+    rm -f "${pe_package_dir}/repodata/repomd.xml.asc"
+    gpg --detach-sign --armor --force-v3-sigs "${pe_package_dir}/repodata/repomd.xml"
+    ;;
+  ubuntu)
+    apt install -y tree vim dpkg-dev
+
+    cp /jpartlow-src/puppet-enterprise-vanagon/output/deb/"$codename"/*.deb "${pe_package_dir}"
+
+    # shellcheck disable=SC2002
+    cat /root/gpg/GPG-KEY-frankenbuilder.pub | apt-key add -
+
+    rm "${pe_package_dir}"/{Release,Release.gpg,Packages,Packages.gz}
+
+    pushd "${pe_package_dir}"
+      dpkg-scanpackages . /dev/null 1> "${pe_package_dir}/Packages"
+      gzip -9c "${pe_package_dir}/Packages" > "${pe_package_dir}/Packages.gz"
+    popd
+
+    get_md5() {
+      local output
+      output=$(md5sum "${pe_package_dir}/$1" | cut --delimiter=' ' --fields=1)
+      echo "$output"
+    }
+
+    get_sha() {
+      local output
+      output=$(sha256sum "${pe_package_dir}/$1" | cut --delimiter=' ' --fields=1)
+      echo "$output"
+    }
+
+    get_bytes() {
+      local output
+      output=$(wc --bytes "${pe_package_dir}/$1" | cut --delimiter=' ' --fields=1)
+      echo "$output"
+    }
+
+    # Generate Release file
+    release_file_contents="Origin: Puppetlabs
+Label: Puppet Enterprise
+Codename: ${codename}
+Architecture: ${os_arch}
+MD5Sum:
+ $(get_md5 "Packages") $(get_bytes "Packages") Packages
+ $(get_md5 "Packages.gz") $(get_bytes "Packages.gz") Packages.gz
+SHA256:
+ $(get_sha "Packages") $(get_bytes "Packages") Packages
+ $(get_sha "Packages.gz") $(get_bytes "Packages.gz") Packages.gz
+"
+
+    remote_release_file="${pe_package_dir}/Release"
+    remote_release_file_asc="${pe_package_dir}/Release.gpg"
+    echo "${release_file_contents}" > "$remote_release_file"
+    chmod 644 "${remote_release_file}"
+
+    gpg --armor --detach-sign --output "${remote_release_file_asc}" "${remote_release_file}"
     ;;
   *)
-    exit 3
+    echo "Unknown platform: ${platform}"
+    exit 2
     ;;
 esac
 
-grep -E '^[^#]*postgres_version_override' "${pe_dir}/conf.d/custom-pe.conf" || exit 2
+grep -E '^[^#]*postgres_version_override' "${pe_dir}/conf.d/custom-pe.conf" || exit 3
 
 # retry pe installation
 /jpartlow-src/integration-tools/scripts/rerun-pe-install-with-module-links.sh -d "${pe_dir}" -m puppet_enterprise,pe_postgresql
