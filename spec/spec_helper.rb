fcbd220da526b5a32e703a1b8336c13d1291b4bd
Josh Partlow <joshua.partlow@puppetlabs.com>
2019-03-29 21:28:56 +0000

    (maint) Remove extra RSpec c.mock_with configuration
    Redundant because we've already set c.mock_with :rspec in the proceeding
    block.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,7 +9,6 @@ RSpec.configure do |c|
   c.mock_with :rspec do |mocks|
     mocks.syntax = :expect
   end
-  c.mock_with :rspec
 end
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'bolt_spec/plans'
