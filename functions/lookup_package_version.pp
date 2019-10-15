b5ea45fe757a550430861b5ce738e468bfba71b5
Josh Partlow <joshua.partlow@puppetlabs.com>
2019-10-15 20:47:29 +0000

    (maint) Adjust lookup_package_version() for non-postgres packages
    ...and to accept packages with .gabc123 git describe in their versions.

--- functions/lookup_package_version.pp
+++ functions/lookup_package_version.pp
@@ -2,6 +2,6 @@ function meep_tools::lookup_package_version(
   Enterprise_tasks::Absolute_path $package_dir,
   String $package_name,
 ) {
-  $find_result = run_command("find ${package_dir} -name ${package_name}* | grep -oE '[.0-9]+-[0-9]' | sort | tail -n1", 'localhost').first()
+  $find_result = run_command("find ${package_dir} -name ${package_name}* | grep -oE '[.0-9]+(\\.rc[0-9]+\\.[0-9]+)?(\\.g[0-9a-f]+)?-[0-9]' | sort | tail -n1", 'localhost').first()
   $find_result.value()['stdout'][0,-2] # strip cr
 }
